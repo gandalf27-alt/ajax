e3bb02ad6cbb93e382cb26eed06eb841eda11e1e
webcodeur <godardfranck@free.fr>
2020-03-02 10:42:26 +0100

    modifi index

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        let xhr = null;
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            try {
+                xhr = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+        } else{
+            alert("Mettre à jour votre navigateur");
+            xhr = false
+        }
+    </script>
+</body>
+</html>
+
+
+
+
+
