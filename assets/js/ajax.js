6f35f40ea5a1109df4ac26d2b6894e4402808856
webcodeur <godardfranck@free.fr>
2020-03-02 14:26:55 +0100

    update

---
+++ assets/js/ajax.js
@@ -0,0 +1,23 @@
+
+
+function ajax() { 
+
+    let xhr = null;
+    
+
+if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+} else if (window.ActiveXObject) {
+    try {
+        xhr = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+} else{
+    alert("Mettre à jour votre navigateur");
+    xhr = false
+}
+
+return xhr;
+
+}
