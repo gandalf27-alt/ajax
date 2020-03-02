5a5ca2189ccb73093ab6891fee9fb5f84ff37038
webcodeur <godardfranck@free.fr>
2020-03-02 14:24:55 +0100

    maj index

--- index.php
+++ index.php
@@ -3,13 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script> src="/assets/assetjs/ajax.js"</script>
+    <script  src="./assets/js/ajax.js"></script>
+    <title>Ajax</title>
+    
 </head>
 <body>
     <script>
-       console.log(ajax());
-    </script>
+       let xhrTest = ajax();
+       let test = function() {
+        xhrTest.onreadystatechange = function(){
+            if (xhrTest.readyState == 4 && xhrTest.status == 200) {
+
+                alert(xhrTest.responseText);
+            }
+
+        };
+        xhrTest.open("POST", "test.php", true);
+        xhrTest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhrTest.send();
+       };
+</script>
+
+
+
+
+<div> 
+       <form action="" method="POST">
+       <div> 
+       <label for="nom">Nom</label>
+       <input type="text" id="nom" name="nom"><br>
+       </div>
+
+       <div> 
+       <label for="prenom">Prenom</label>
+       <input type="text"id="prenom" name="prenom" ><br>
+       </div>
+
+       <div> </div>
+       <label for="email">Email</label>
+       <input type="email" id="email" name="email" ><br>
+       </div>   
+           
+            <div>
+
+            <input type="reset" value="Effacer" />
+            <input type="submit" value="submitted" onsubmit="test()" />
+        </div>   
+       </div>      
+       
+       
+       
+       
+       </form>
+
+    
+
+    <p onclick="test()"> </P>
+
 </body>
 </html>
 
